6ec106e8f3a7ff1689ff1df17cdf30a339f193f8
Ewan Pettigrew <ewanpettigrew@outlook.com>
2026-03-14 23:05:28 +1030

    Enhance OscilloscopeTab with new features and UI elements
    Added new methods for importing vectors and exporting WAV files. Introduced additional UI components for custom mode and 3D rotation.

--- oscilloscopetab.h
+++ oscilloscopetab.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <vector>
 #include <QString>
+#include <QTimer>
 
 class QSlider;
 class QSpinBox;
@@ -18,6 +19,7 @@ class OscilloscopePlot : public QWidget {
 public:
     explicit OscilloscopePlot(QWidget *parent = nullptr);
     void setParameters(int fx, int fy, float phase);
+    void setCustomVectors(const std::vector<float>& x, const std::vector<float>& y);
 
 protected:
     void paintEvent(QPaintEvent *event) override;
@@ -26,7 +28,12 @@ private:
     int m_fx = 1;
     int m_fy = 1;
     float m_phase = 0.0f;
+    std::vector<float> m_customX;
+    std::vector<float> m_customY;
+    bool m_useCustom = false;
 };
+
+struct Vec3 { float x, y, z; };
 
 class OscilloscopeTab : public QWidget {
     Q_OBJECT
@@ -38,9 +45,14 @@ signals:
 
 private slots:
     void updateParameters();
-    void generateTimbre();
+    //void generateTimbre();
     void loadPreset(int index);
     void generateString();
+    void importVectors();
+    void importObj();
+    void updateRotation();
+    void exportWav();
+    void updateCsvScale();
 
 private:
     QComboBox* presetCombo;
@@ -51,12 +63,37 @@ private:
     QSlider* phaseEndSlider;
     QDoubleSpinBox* timeSpin;
     QCheckBox* pingPongCheck;
+    QComboBox* buildModeCombo;
 
     QTextEdit* stringOutput;
 
     OscilloscopePlot* plotWidget;
-    QPushButton* generateBtn;
     QPushButton* generateStringBtn;
+    QPushButton* importBtn;
+    QPushButton* importObjBtn;
+    QTimer* rotationTimer;
+    QPushButton* exportWavBtn;
+    QSlider* objectScaleSlider;
+
+    bool m_isCustomMode = false;
+    std::vector<float> m_activeX;
+    std::vector<float> m_activeY;
+
+    std::vector<Vec3> m_vertices3D;
+    std::vector<int> m_drawPath;
+
+    float m_angleX = 0.0f;
+    float m_angleY = 0.0f;
+
+    QCheckBox* vibrateCheck;
+    QCheckBox* loopSweepCheck;
+    QCheckBox* rotateCustomCheck;
+    QCheckBox* rotate3dCheck;
+
+    QSlider* csvScaleSlider;
+    std::vector<float> m_originalX;
+    std::vector<float> m_originalY;
+
 };
 
 #endif // OSCILLOSCOPETAB_H
